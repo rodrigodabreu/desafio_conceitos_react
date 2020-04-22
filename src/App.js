06425d9f9a6cd8df8cd07c9d836616e090d6e5a5
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2020-04-22 20:14:18 -0300

    feature: adiciona lógica para adição de um novo repositório

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title: `Repositório - ${Date.now()}`,
+      url: "https://github.com/Rocketseat/umbriel",
+      techs: ["Node", "Express", "TypeScript"],
+      likes: 0,
+    });
+
+    const responseRepositories = response.data;
+    console.log(responseRepositories);
+    setRepositories([...repositories, responseRepositories]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.get("repositories").then((response) => {
+      const { repositories } = response.data;
+      console.log(response.data);
+      console.log(repositories);
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
+        {repositories.map((repository) => (
+          <li key={repository.id}>{repository.title}</li>
+        ))}
         <li>
           Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
+          <button onClick={() => handleRemoveRepository(1)}>Remover</button>
         </li>
       </ul>
 
